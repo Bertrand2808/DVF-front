a52a2360e8ecb448feb6b95210676acbb89efd9b
Bertrand2808 <bertrand2808@icloud.com>
2023-12-21 15:26:04 +0100

    add esgi logo, add some tailwind features

--- dvf/src/Formulaire.jsx
+++ dvf/src/Formulaire.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
-
+import {HomeModernIcon} from '@heroicons/react/24/outline'
+import logo from './assets/logo.png'
 export default function Formulaire() {
   const [latitude, setLatitude] = useState('')
   const [longitude, setLongitude] = useState('')
@@ -35,24 +36,27 @@ export default function Formulaire() {
 
   return (
     <div className=''>
-      <div className="p-3 text-xl text-center font-bold">
-        <h1>Explorateur de données de valeurs foncières</h1>
+      <div className="p-3 flex text-xl text-center font-bold border border-black justify-center">
+        <img src={logo} alt="logo" className="mx-auto mr-10 w-20"/>
+        <h1 className="mx-auto flex-grow">Explorateur de données de valeurs foncières</h1>
+        <div className="w-20 mr-10 invisible"> {/* Élément factice pour équilibrage */}</div>
       </div>
-      <div className='bg-blue-50 h-screen flex items-center'>
-        <h2 className="flex items-center text-center">test</h2>
-        <form className="w-64 mx-auto p-2 mb-12" onSubmit={handleSubmit}>
+      <div className='bg-blue-50 h-screen flex-row items-center'>
+        <HomeModernIcon className="w-28 h-25 mx-auto text-blue-700 pt-7"/>
+        <h2 className="text-2xl text-center font-bold text-blue-700">Rechercher les transactions par géolocalisation</h2>
+        <form className="w-64 mx-auto p-2 mb-12 mt-4 max-w-sm" onSubmit={handleSubmit}>
           <input value={latitude}
                   onChange={ev => setLatitude(ev.target.value)}
                   type="text" placeholder="Latitude"
-                  className="block w-full rounded-sm p-2 mb-2 border"/>
+                  className="block w-full p-2 mb-2 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 "/>
           <input value={longitude}
                   onChange={ev => setLongitude(ev.target.value)}
                   type="text" placeholder="Longitude"
-                  className="block w-full rounded-sm p-2 mb-2 border"/>
+                  className="block w-full p-2 mb-2 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 "/>
           <input value={rayon}
                   onChange={ev => setRayon(ev.target.value)}
                   type="text" placeholder="Rayon"
-                  className="block w-full rounded-sm p-2 mb-2 border"/>
+                  className="block w-full p-2 mb-2 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 "/>
           <button className="bg-blue-500 text-white block w-full rounded-sm">Soumettre</button>
         </form>
       </div>
